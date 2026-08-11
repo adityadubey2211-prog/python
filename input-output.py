eb339bec08559071772c9f67318461af86bc8c74
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-08-11 18:00:10 +0530

    input-output statements

---
+++ input-output.py
@@ -0,0 +1,7 @@
+a = input("enter a number:")
+print("the above input number is ->", a)
+print(type(a))
+# input statement allways take string as default data type
+b = int(input("enter a numbeer:"))# after type-casting b became int from str:
+print("the above input number is ->", b)
+print(type(b))
