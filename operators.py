8af98405cbe93a8899e157fc7028a35b910fbe2d
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-08-11 17:35:48 +0530

    operators  and variables in python

---
+++ operators.py
@@ -0,0 +1,15 @@
+# operators in python 
+a = 100
+b = 10
+c = True
+d = False
+print(a-b)# output -> 90
+print(a+b)# output -> 110
+print(a/b)# output -> 10.0 (/) gives an float value
+print(a*b)# output -> 1000
+print(a//b)# output -> 10 (//) gives an int value
+print(a%b)# output -> 0
+print(c or d)# output -> true
+print(c and d)# output -> false
+print(not c)# output -> false
+print(not d)# output -> true
---
+++ veriables.py
@@ -0,0 +1,18 @@
+name = "Aditya dubey"# name is an string so Aditya dubey is in "" -> "Aditya dubey":
+age = 18
+earnings = 123456789.224354
+# dont use print("age") -> this instruction prints "age":
+print(age) # this instruction print age value
+print("my name is:", name,"\n","my age is:", age)# "\n" use for next line && "\t" use for space:
+
+# how we can access the type of variables:
+print(type(name))
+print(type(age))
+print(type(earnings))
+
+# addition and  subtraction of two numbers:
+a = 20
+b = 40
+add = a + b
+diff = b - a
+print("addition:", add, "\t", "subtraction", diff)
