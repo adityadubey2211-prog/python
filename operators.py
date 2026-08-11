386985e9cfda30bcad88aae6acf5b1014bdf4c37
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-08-11 17:50:42 +0530

    operators in python

--- operators.py
+++ operators.py
@@ -13,3 +13,23 @@ print(c or d)# output -> true
 print(c and d)# output -> false
 print(not c)# output -> false
 print(not d)# output -> true
+print(a == b)# output ->false
+print(a != b)# output ->true
+print(a > b)# output ->true
+print(a >= b)# output ->true
+print(a < b)# output ->false
+print(a <= b)# output ->false
+
+
+e = a # value of is assigne to e
+print(e)#output -> 100
+e += 100
+print(e) #output -> 200
+e -= 100
+print(e)# output -> 100
+e *= 2
+print(e)#output -> 200
+e /= 2
+print(e)#output -> 100.0
+e //= 2
+print(int(e)) # output -> 50
