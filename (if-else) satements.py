4e63c8f887b1d2968636d21fdbc65f1f41f095f6
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-08-12 06:37:07 +0530

    learn about if-else condition in python

---
+++ (if-else) satements.py
@@ -0,0 +1,15 @@
+age = int(input("Enter the age :"))
+if(age >= 18):
+    print("you can vote")
+else:
+    print("you can not vote")
+
+
+#elif
+color = "green"
+if(color == "red"):
+    print("stop")
+elif(color == "green"):
+    print("go")
+else:
+    print("look")
