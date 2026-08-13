0cd4a9738e4826d492a4f6adfe2f7f6832b9bb6f
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-08-13 18:52:59 +0530

    list in python

---
+++ List and Tupple.py
@@ -0,0 +1,19 @@
+List = [1, 2, 3 ,4, "HELLO"]
+print(type(List))
+print(List)
+print(List[0])
+print(List[1])
+List.append(5)
+print(List)
+
+nums = [1,2,1,3,4,3,6]
+print(nums)
+nums.sort()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+nums.remove(1)
+print(nums)
+print(nums[0:4])
+nums.pop(2)
+print(nums)
